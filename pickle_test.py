bee4e6e5ca829fe8f33f540e2dd85aea0f5276c6
Ranjeetrk <31475498+Ranjeetrk@users.noreply.github.com>
2019-04-16 11:55:53 +0530

    Update pickle_test.py

--- pickle_test.py
+++ pickle_test.py
@@ -27,23 +27,22 @@ def unpickle(file):
     return img_representation
 
 
-pickle_vector('rishabh.jpg', 'rishi')
+
 pickle_vector('ranjeet.jpg', 'ranj')
-pickle_vector('suraj.jpg', 'suri')
-pickle_vector('tathagat.jpg', 'tath')
+
 
 """base = os.path.abspath(os.path.dirname(__file__))
 base = os.path.join(base, "face_vectors")
 
-base1 = os.path.join(base, 'rishi')
-base2 = os.path.join(base, 'rishi29')
+base1 = os.path.join(base, 'rk')
+base2 = os.path.join(base, 'rk29')
 img1_representation = unpickle(base1+'.pkl')
 img2_representation = unpickle(base2+'.pkl')
 
 
 euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
 
-if euclidean_distance < 120:
+if euclidean_distance < 45:
     print("verified... they are same person")
 else:
     print("unverified! they are not same person!")"""
